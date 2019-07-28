a631ffcc5121a6a4625ec7948aba3ca15c0c780d
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2019-07-28 23:54:22 +0300

    [implement-product-images]  - fixed: tests

--- src/app/shared/components/swiper-slider/swiper-slider.component.spec.ts
+++ src/app/shared/components/swiper-slider/swiper-slider.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SwiperSliderComponent } from './swiper-slider.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('SwiperSliderComponent', () => {
   let component: SwiperSliderComponent;
@@ -9,6 +10,7 @@ describe('SwiperSliderComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [SwiperSliderComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     })
     .compileComponents();
   }));
