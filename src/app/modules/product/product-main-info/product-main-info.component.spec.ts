82abefae6b29f7ccb570635c3c13933b0c4c4f79
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2019-08-25 19:55:49 +0300

    fix tests

--- src/app/modules/product/product-main-info/product-main-info.component.spec.ts
+++ src/app/modules/product/product-main-info/product-main-info.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductMainInfoComponent } from './product-main-info.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('ProductMainInfoComponent', () => {
   let component: ProductMainInfoComponent;
@@ -9,6 +10,7 @@ describe('ProductMainInfoComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ProductMainInfoComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     })
     .compileComponents();
   }));
