7acf75ccbec2badf2d9eae569293386dcee47226
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2019-09-20 00:58:25 +0300

    fix tests

--- src/app/shared/components/product-price/product-price.component.spec.ts
+++ src/app/shared/components/product-price/product-price.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductPriceComponent } from './product-price.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('ProductPriceComponent', () => {
   let component: ProductPriceComponent;
@@ -9,6 +10,7 @@ describe('ProductPriceComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ProductPriceComponent],
+      imports: [TranslateModule.forRoot()],
     })
     .compileComponents();
   }));
