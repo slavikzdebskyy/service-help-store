7f652b364ee04d28a7eaa58dd8f3b2c2c324d764
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2019-10-06 13:28:08 +0300

    [implement-apollo-and-product-service]  - update Angular CLI to 8 version  - implement Service to get products  - implement apollo-angular for graphql  - implement graphql.module.ts

---
+++ src/app/graphql.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { environment } from './../environments/environment';
+
+const uri: string = environment.graphqlURL;
+export function createApollo(httpLink: HttpLink): any {
+  return {
+    link: httpLink.create({ uri }),
+    cache: new InMemoryCache(),
+  };
+}
+
+@NgModule({
+  exports: [
+    ApolloModule,
+    HttpLinkModule,
+  ],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ],
+})
+export class GraphQLModule {}
